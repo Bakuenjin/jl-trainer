12bf7e21f5cfb6c87b7898a29aa118e8fa3ed82c
Julian Zameit <julian.zameit.96@gmail.com>
2020-03-25 22:02:52 +0100

    changed replaceAll default value to truthy

--- src/utils/replace-utils.ts
+++ src/utils/replace-utils.ts
@@ -8,7 +8,8 @@ export function replacePattern(text: string, from: string, to: string): string {
 
 export function replacePatternList(text: string, replaceConfig: ReplaceConfig) {
 	replaceConfig.forEach(config => {
-        text =  config.replaceAll ?
+		const replaceAll = (typeof config.replaceAll !== 'boolean' || config.replaceAll)
+        text = replaceAll ?
             replacePattern(text, config.from, config.to) :
             text.replace(config.from, config.to)
 	})
