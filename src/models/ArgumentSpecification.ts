936c715a38d6153d0a6417c75f291de9de716fea
Julian Zameit <julian.zameit.96@gmail.com>
2020-03-26 18:10:32 +0100

    removed Describable class

--- src/models/ArgumentSpecification.ts
+++ src/models/ArgumentSpecification.ts
@@ -1,13 +1,10 @@
-import Describable from "./Describable"
-
-export default class ArgumentSpecification extends Describable {
+export default class ArgumentSpecification {
 
     public readonly name: string
     public readonly description: string
     public readonly required: boolean
 
     constructor(name: string, description: string, required: boolean) {
-        super()
         this.name = name
         this.description = description
         this.required = required
--- src/models/Describable.ts
+++
@@ -1,6 +0,0 @@
-export default abstract class Describable {
-
-    public abstract readonly name: string
-    public abstract readonly description: string
-
-}
